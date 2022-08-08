608d2e844457cf0388496c4bfd3760818de80b93
kkyu0718 <net605@naver.com>
2022-08-09 01:39:59 +0900

    heroku : package.json scripts 수정

--- server/index.js
+++ server/index.js
@@ -33,12 +33,12 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cookieParser());
 
-app.use('/api/users', require('./routes/users'));
+// app.use('/api/users', require('./routes/users'));
 
 
 //use this to show the image you have in node js server to client (react js)
 //https://stackoverflow.com/questions/48914987/send-image-path-from-node-js-express-server-to-react-client
-app.use('/uploads', express.static('uploads'));
+// app.use('/uploads', express.static('uploads'));
 
 // Serve static assets if in production
 if (process.env.NODE_ENV === "production") {
