1212c460e804e06ab93186600a9cccb4d475fc98
kkyu0718 <net605@naver.com>
2022-08-29 15:59:38 +0900

    jwt.guard.ts 추가

--- server/src/boards/boards.controller.ts
+++ server/src/boards/boards.controller.ts
@@ -1,9 +1,10 @@
 import { Body, Controller, Post, UseGuards } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
+import { JwtAuthGuard } from 'src/auth/jwt.guard';
 import { BoardsService } from './boards.service';
 
 @Controller('boards')
-@UseGuards(AuthGuard('jwt'))
+@UseGuards(JwtAuthGuard)
 export class BoardsController {
     constructor(
         private boardsService: BoardsService
